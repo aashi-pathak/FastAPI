da009582a6add246f635dd4e1922faf163ba2631
aashi-pathak <aashipathak1103@gmail.com>
2024-08-04 12:22:35 +0900

    blogs detail view

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-from db.repository.blog import list_blogs
+from db.repository.blog import list_blogs, retreive_blog
 from db.session import get_db
 
 
@@ -14,3 +14,8 @@ def home(request: Request, db: Session=Depends(get_db)):
     blogs = list_blogs(db=db)
     context = {"request": request, "blogs": blogs}
     return templates.TemplateResponse("blogs/home.html", context=context)
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session=Depends(get_db)):
+    blog = retreive_blog(id=id, db=db)
+    return templates.TemplateResponse("blogs/detail.html", {"request": request, "blog": blog})
